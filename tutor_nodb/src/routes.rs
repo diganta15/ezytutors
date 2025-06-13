5b1220f8c15f62f95f6934e723f03a38dad2dec9
Diganta Das <ddas461598@gmail.com>
2025-06-14 00:43:59 +0530

    Added tests for adding a course, fixed error with routes

--- tutor_nodb/src/routes.rs
+++ tutor_nodb/src/routes.rs
@@ -1,13 +1,10 @@
-use crate::handlers::{health_check_handler,new_course};
+use crate::handlers::{ health_check_handler, new_course };
 use actix_web::web;
 
-
-pub fn general_routes(cfg:&mut web::ServiceConfig){
+pub fn general_routes(cfg: &mut web::ServiceConfig) {
     cfg.route("/health", web::get().to(health_check_handler));
 }
 
-pub fn course_routes(cfg:&mut web::ServiceConfig){
-    cfg
-    .service(web::scope("/courses"))
-    .route("/",web::post().to(new_course));
+pub fn course_routes(cfg: &mut web::ServiceConfig) {
+    cfg.service(web::scope("/courses").route("/", web::post().to(new_course)));
 }
