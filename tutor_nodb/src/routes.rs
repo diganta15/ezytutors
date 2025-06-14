af7e2ead292d71714c92fdfd457e2fb6969da4aa
Diganta Das <ddas461598@gmail.com>
2025-06-14 09:33:24 +0530

    Added route and handler to get course by tutor

--- tutor_nodb/src/routes.rs
+++ tutor_nodb/src/routes.rs
@@ -1,4 +1,4 @@
-use crate::handlers::{ health_check_handler, new_course };
+use crate::handlers::{ health_check_handler, new_course,get_courses_for_tutor };
 use actix_web::web;
 
 pub fn general_routes(cfg: &mut web::ServiceConfig) {
@@ -6,5 +6,11 @@ pub fn general_routes(cfg: &mut web::ServiceConfig) {
 }
 
 pub fn course_routes(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::scope("/courses").route("/", web::post().to(new_course)));
+    cfg.service(web::scope("/courses")
+    .route("/", web::post().to(new_course))
+    .route("/{tutor_id}",web::get().to(get_courses_for_tutor))
+
+);
+    
 }
+
